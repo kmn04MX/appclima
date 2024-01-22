d6dc274fa49aa1ad6df8ac14e8de111ce9a9791c
Felipe Arroyo <afelipef7@gmail.com>
2024-01-22 01:01:00 -0600

    finalizo la aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 
 
 import { CssBaseline } from '@mui/material'
@@ -10,10 +10,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
 )
